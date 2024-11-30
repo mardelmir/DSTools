e3bd56d828db5c085520c3b249f20d9b0387a664
María DM <mariad1996@gmail.com>
2024-11-30 14:37:24 +0100

    second correction

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dstoolz',
-    version = '0.1',
-    packages = find_packages()
+    version = '0.1.0',
+    packages = find_packages(),  # Encuentra automáticamente `dstoolz`
+    install_requires = [],  # Dependencias necesarias, si las hay
+    description = 'Data Science Tools',
+    author = 'mardelmir',
+    url = 'https://github.com/mardelmir/DSTools',
 )
